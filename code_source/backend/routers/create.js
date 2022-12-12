4d3335857da526f958ec83e7f759d5803b175b18
Nathan Lesourd <nlesourd@pc-czc22790km.ad.insa-rouen.fr>
2022-12-12 13:53:39 +0100

    create canva fonctionne avec maj BD mais pas de redirection vers canva create ni de gestion du statut

--- code_source/backend/routers/create.js
+++ code_source/backend/routers/create.js
@@ -15,56 +15,55 @@ const db  = new sqlite3.Database('./db/pixel_war.db', (err) => {
 });
 
 router.post('/available', function(req, res) {
-	console.log("passe1");
     let data = req.body;
     db.serialize(() => {
-        db.get("SELECT nom FROM canva", (err, result) => {
-			if(err){
-				next(err);
-			} else {
-				if(typeof(result)==="undefined"){
+        const statement = db.prepare("SELECT nom FROM canva WHERE nom=?");
+        statement.get(data['nom'], (err, result) => {
+            if(err){
+                next(err);
+            } else {
+                if(typeof(result)==="undefined"){
                     res.status(200).send("OK");
                 } else{
                     res.status(200).send("KO");
                 }
-			}
-		});
-		statement.finalize();
+            }
+        });
+        statement.finalize();
     })
 })
 
 router.post('/', function (req, res, next) {
 	let data = req.body;
     //console.log(data);
-	if(data['name']!=null && data['name']!="" && data['theme']!=null && data['theme']!="" && data['width']!=null && data['width']!="" && data['height']!=null && data['height']!=""){
-
-		db.serialize(() => {
-			// check if the name is okay
-			db.get("SELECT nom FROM canva", (err, result) => {
+	if(data['nom']!=null && data['nom']!="" && data['theme']!=null && data['theme']!="" && data['width']!=0 && data['height']!=0){
+        db.serialize(() => {
+			const statement = db.prepare('INSERT INTO canva (nom, theme, longueur, largeur) VALUES(?, ?, ?, ?);');
+			statement.get(data['nom'], data['theme'], data['width'], data['height'], (err, result) => {
 				if(err){
-					next(err);
+					res.status(400).send('Name already used'); //faire une page propre
 				} else {
-					if (typeof(result)==="undefined"){ //nom non utilisé -> creation du canva
-						let name = data['name'];
-                        let url_name = "/canva/" + name;
-                        console.log(url_name);
-                        //res.redirect(url_name); //page à créer
-					}
-					else{
-						res.write("<p> canva's name already used </p>");
+                    if(req.session.loggedin){
+						db.get('SELECT * FROM site;', (err, result) => {
+							nbCanvaAvant = result["nbCanvaTotal"];
+							incrementerNbCanva(nbCanvaAvant);
+						});
+						res.render('login.ejs', {logged: true, login: req.session.login, error: true});
 					}
 				}
 			});
 			statement.finalize();
-		
-		});
-
+        });
 	} else {
 		res.status(400).send('Bad request!');
 	}
 });
 
-
+function incrementerNbCanva(nbCanvaAvant){   
+	const statement = db.prepare('UPDATE site SET nbCanvaTotal = ?;');
+	statement.get(nbCanvaAvant + 1, (err, result) => {});
+	statement.finalize();
+}
 
 router.get('/', function (req, res) {
 	res.sendFile('create.html', {root: "../frontend"});
