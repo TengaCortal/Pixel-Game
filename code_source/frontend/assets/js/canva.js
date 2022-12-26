f5ff3f88a814aa0edc93cb58a6d499f600ee65fc
Pasquesoone Adrien <adrien.pasquesoone@insa-rouen.fr>
2022-12-26 23:45:37 +0100

    affichage par défaut des pixels en blanc

--- code_source/frontend/assets/js/canva.js
+++ code_source/frontend/assets/js/canva.js
@@ -6,14 +6,13 @@ var duration = duree;
 
 var interval;
 
+//partie canva
+const context = SUPER.getContext('2d');
+
 //On définit la taille du canva
 const tailleCellule = 10;
 SUPER.width= tailleCellule*width;
 SUPER.height= tailleCellule*height;
-// var columns = width;
-// var rows = height
-
-
 
 
 //liste des couleurs qui compose la palette à laquelle l'utilisateur pourra accéder rapidement 
@@ -34,17 +33,8 @@ palette.forEach(color => {
 
     couleur.addEventListener('click', () => {
         couleurChoisie = color 
-        //couleur.innerHTML = `<i class="fa-solid fa-check"></i>`
     })
-})  
-
-//partie canva
-const context = SUPER.getContext('2d');
-
-
-/*curseur.addEventListener('click', function(event) {
-    addPixel()
-})*/
+})
 
 SUPER.addEventListener('click', function(){
     [x, y] = addPixel()
@@ -60,7 +50,6 @@ SUPER.addEventListener('click', function(){
 
     // Récupérer l'élément overlay
     var overlay = document.querySelector('.overlay');
-    
 
     // Exécuter la fonction qui supprime l'overlay une fois le minuteur terminé
     setTimeout(function() {
@@ -73,30 +62,6 @@ SUPER.addEventListener('click', function(){
     interval = setInterval(startTimer, 1000);
 })
 
-// function drawGrid(){
-//     context.beginPath()
-
-//     // Iterate through rows and columns
-//     for (let i = 0; i < rows; i++) {
-//       for (let j = 0; j < columns; j++) {
-//         // Calculate x and y positions
-//         const x = j * tailleCellule;
-//         const y = i * tailleCellule;
-//         // Draw vertical line
-//         context.beginPath();
-//         context.moveTo(x, 0);
-//         context.lineTo(x, SUPER.height);
-//         //context.stroke();
-//         // Draw horizontal line
-//         context.beginPath();
-//         context.moveTo(0, y);
-//         context.lineTo(SUPER.width, y);
-//         //context.stroke();
-//       }
-//     }    
-// }
-
-var pixelPresent;
 var ancienX;
 var ancienY;
 var ancienneCouleur
@@ -104,42 +69,33 @@ var ancienneCouleur
 function drawMouseEffect(x, y) {
     // Set fill color
     context.fillStyle = couleurChoisie;
-    
 
     // Calculate grid position
     const gridX = Math.floor(x / tailleCellule) * tailleCellule;
     const gridY = Math.floor(y / tailleCellule) * tailleCellule;
     ancienX = gridX;
     ancienY = gridY;
+
+    //Store the values when the mouse will quit the pixel we are on
+    r = context.getImageData(gridX, gridY, 1, 1).data[0]
+    g = context.getImageData(gridX, gridY, 1, 1).data[1]
+    b = context.getImageData(gridX, gridY, 1, 1).data[2]
+    ancienneCouleur = rgbToHex(r,g,b); 
+
     // Fill grid square
-    if (context.getImageData(gridX, gridY, 1, 1).data[3]==0){
-        pixelPresent = false;
-    }
-    else{
-        pixelPresent = true;
-        r = context.getImageData(gridX, gridY, 1, 1).data[0]
-        g = context.getImageData(gridX, gridY, 1, 1).data[1]
-        b = context.getImageData(gridX, gridY, 1, 1).data[2]
-        ancienneCouleur = rgbToHex(r,g,b);
-    }
-    context.fillRect(gridX, gridY, tailleCellule, tailleCellule);
-  }
+    context.fillRect(gridX, gridY, tailleCellule, tailleCellule);      
+}
 
 SUPER.addEventListener('mousemove', function(event){
-    if (pixelPresent){
-        context.beginPath();
-        context.fillStyle = ancienneCouleur
-        context.fillRect(ancienX, ancienY, tailleCellule, tailleCellule)
-    }
-    else{
-        // Clear canvas
-        context.clearRect(ancienX, ancienY, tailleCellule, tailleCellule);
-    }
-    // Draw grid
-    //drawGrid();
+    //redraw the previous pixel we were on
+    context.beginPath();
+    context.fillStyle = ancienneCouleur
+    context.fillRect(ancienX, ancienY, tailleCellule, tailleCellule)
+
     // Get mouse position
     const x = event.offsetX;
     const y = event.offsetY;
+
     // Draw mouse effect
     drawMouseEffect(x, y);
 })
@@ -156,7 +112,6 @@ function addPixel(){
 }
 
 //affichage au chargement
-//drawGrid()
 
 if (getCookie(`timer${login}`)>0){
     interval = setInterval(startTimer, 1000);
@@ -180,6 +135,23 @@ if (getCookie(`timer${login}`)>0){
     }, 1000*getCookie(`timer${login}`)); // durée voulue
 }
 
+var pixelBlanc = context.createImageData(tailleCellule, tailleCellule);
+for (let j = 0; j < pixelBlanc.data.length; j += 4) {
+    // Modify pixel data
+    pixelBlanc.data[j+0] = 255;
+    pixelBlanc.data[j+1] = 255;
+    pixelBlanc.data[j+2] = 255;
+    pixelBlanc.data[j+3] = 255;
+}
+
+//initialisation par défaut à tous les pixels blancs
+for (let i = 0; i < width; i+=1){
+    for (let j = 0; j < height; j+=1){
+        context.putImageData(pixelBlanc, i*tailleCellule, j*tailleCellule)
+    }
+}
+
+//affichage des pixels qui sont en BD
 for (let i = 0; i < matrice.length; i+=5){
         pixel = context.createImageData(tailleCellule, tailleCellule);
 
@@ -237,46 +209,3 @@ function componentToHex(c) {
 function rgbToHex(r, g, b) {
     return "#" + componentToHex(r) + componentToHex(g) + componentToHex(b);
 }
-
-  /* 
-First, create a canvas element in your HTML file:
-<canvas id="myCanvas" width="500" height="300"></canvas>
-
-Next, use JavaScript to get a reference to the canvas element and its drawing context:
-var canvas = document.getElementById("myCanvas");
-var context = canvas.getContext("2d");
-
-Then, use the canvas drawing context to draw on the canvas. For example, you can use the fillRect method to draw a filled rectangle:
-context.fillRect(10, 10, 50, 50);
-
-Once you have drawn on the canvas, you can convert the canvas pixels to a data URL using the toDataURL method:
-var dataURL = canvas.toDataURL();
-
-To store the data URL in a database, you will need to send it to a server-side script using an HTTP request (such as an AJAX request). The server-side script can then store the data URL in the database.
-Here is an example of how you could send the data URL to a server-side script using an AJAX request:
-
-$.ajax({
-  type: "POST",
-  url: "save_image.php",
-  data: {
-    dataURL: dataURL
-  }
-});
-
-On the server side, you can use a server-side language like PHP to receive the data URL and store it in a database. Here is an example of how you could do this using PHP:
-
-<?php
-
-// Connect to the database
-$db = new mysqli("localhost", "username", "password", "database");
-
-// Escape the data URL to prevent SQL injection attacks
-$dataURL = $db->real_escape_string($_POST["dataURL"]);
-
-// Insert the data URL into the database
-$query = "INSERT INTO images (data_url) VALUES ('$dataURL')";
-$result = $db->query($query);
-
-?>
-
-*/
